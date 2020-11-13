8fe62f166eccaeb7b80ab4f49ccb88c29489aa31
Phong <phongntgamedev@gmail.com>
2020-11-13 17:13:55 +0700

    Experience System Start

--- Scenes/Player/Stats/Progress.cs
+++ Scenes/Player/Stats/Progress.cs
@@ -3,26 +3,26 @@ using System;
 
 public class Progress : TextureRect
 {
-    Label damage;
-    Label points;
+	Label damage;
+	Label points;
 
-    public override void _Ready()
-    {
-        damage = (Label)GetNode("Damage");
-        points = (Label)GetNode("Points");
+	public override void _Ready()
+	{
+		damage = (Label)GetNode("Damage");
+		points = (Label)GetNode("Points");
 
-        damage.Text = Global.PlayerStats.Damage.ToString();
-        points.Text = Global.PlayerStats.Points.ToString();
-        Global.PlayerStats.Connect(nameof(PlayerStats.PointsChange), this, nameof(_on_Player_points_changed));
-        Global.PlayerStats.Connect(nameof(PlayerStats.DamageChange), this, nameof(_on_Player_damage_changed));
-    }
+		damage.Text = Global.PlayerStats.Damage.ToString();
+		points.Text = Global.PlayerStats.Points.ToString();
+		Global.PlayerStats.Connect(nameof(PlayerStats.PointsChange), this, nameof(_on_Player_points_changed));
+		Global.PlayerStats.Connect(nameof(PlayerStats.DamageChange), this, nameof(_on_Player_damage_changed));
+	}
 
-    public void _on_Player_points_changed()
-    {
-        points.Text = Global.PlayerStats.Points.ToString();
-    }
-    public void _on_Player_damage_changed()
-    {
-        damage.Text = Global.PlayerStats.Damage.ToString();
-    }
+	public void _on_Player_points_changed()
+	{
+		points.Text = Global.PlayerStats.Points.ToString();
+	}
+	public void _on_Player_damage_changed()
+	{
+		damage.Text = Global.PlayerStats.Damage.ToString();
+	}
 }
